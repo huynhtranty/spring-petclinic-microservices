ccfb717a65d47a2bb19b7f28be62a7985b3e3c3c
hytaty <huynhtranty@gmail.com>
2025-05-24 16:31:43 +0700

    add more cmt

--- spring-petclinic-vets-service/src/main/java/org/springframework/samples/petclinic/vets/model/VetRepository.java
+++ spring-petclinic-vets-service/src/main/java/org/springframework/samples/petclinic/vets/model/VetRepository.java
@@ -20,7 +20,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 /**
  * Repository class for <code>Vet</code> domain objects All method names are compliant with Spring Data naming
  * conventions so this interface can easily be extended for Spring Data See here: http://static.springsource.org/spring-data/jpa/docs/current/reference/html/jpa.repositories.html#jpa.query-methods.query-creation
- *
+ * My name is Ty
  * @author Ken Krebs
  * @author Juergen Hoeller
  * @author Sam Brannen
