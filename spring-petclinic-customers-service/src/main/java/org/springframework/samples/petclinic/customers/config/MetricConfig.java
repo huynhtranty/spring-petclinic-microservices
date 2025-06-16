7cd3d0109ee731f3dfb1aec0195e46bbb6b64f0a
Huynh Tran Ty <22120418@student.hcmus.edu.vn>
2025-06-16 15:04:26 +0700

    add

--- spring-petclinic-customers-service/src/main/java/org/springframework/samples/petclinic/customers/config/MetricConfig.java
+++ spring-petclinic-customers-service/src/main/java/org/springframework/samples/petclinic/customers/config/MetricConfig.java
@@ -1,7 +1,11 @@
 package org.springframework.samples.petclinic.customers.config;
 
+import brave.Tracer;
 import io.micrometer.core.aop.TimedAspect;
 import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.config.MeterFilter;
+import io.micrometer.core.instrument.Meter;
+import io.micrometer.core.instrument.Tag;
 import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,13 +14,28 @@ import org.springframework.context.annotation.Configuration;
 public class MetricConfig {
 
   @Bean
-  MeterRegistryCustomizer<MeterRegistry> metricsCommonTags() {
-      return registry -> registry.config().commonTags("application", "petclinic");
+  public MeterRegistryCustomizer<MeterRegistry> metricsCommonTags() {
+    return registry -> registry.config()
+        .commonTags("application", "customers-service");
   }
 
   @Bean
-  TimedAspect timedAspect(MeterRegistry registry) {
-    return new TimedAspect(registry);
+  public MeterFilter traceIdDynamicTagFilter(Tracer tracer) {
+    return new MeterFilter() {
+      @Override
+      public Meter.Id map(Meter.Id id) {
+        String traceId = "none";
+        var span = tracer.currentSpan();
+        if (span != null && span.context() != null) {
+          traceId = span.context().traceIdString();
+        }
+        return id.withTag(Tag.of("traceId", traceId));
+      }
+    };
   }
 
+  @Bean
+  public TimedAspect timedAspect(MeterRegistry registry) {
+    return new TimedAspect(registry);
+  }
 }
--- spring-petclinic-visits-service/src/main/java/org/springframework/samples/petclinic/visits/config/MetricConfig.java
+++ spring-petclinic-visits-service/src/main/java/org/springframework/samples/petclinic/visits/config/MetricConfig.java
@@ -1,22 +1,42 @@
 package org.springframework.samples.petclinic.visits.config;
 
-import io.micrometer.core.aop.TimedAspect;
+import brave.Tracer;
 import io.micrometer.core.instrument.MeterRegistry;
-import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
+import io.micrometer.core.instrument.config.MeterFilter;
+import io.micrometer.core.instrument.Tag;
+import io.micrometer.core.instrument.Meter;
+import io.micrometer.core.aop.TimedAspect;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 
 @Configuration
 public class MetricConfig {
 
   @Bean
-  MeterRegistryCustomizer<MeterRegistry> metricsCommonTags() {
-    return registry -> registry.config().commonTags("application", "petclinic");
+  public MeterRegistryCustomizer<MeterRegistry> metricsCommonTags() {
+      return registry -> registry.config()
+          .commonTags("application", "visits-service");
   }
 
   @Bean
-  TimedAspect timedAspect(MeterRegistry registry) {
-    return new TimedAspect(registry);
+  public MeterFilter traceIdDynamicTagFilter(Tracer tracer) {
+    return new MeterFilter() {
+      @Override
+      public Meter.Id map(Meter.Id id) {
+        String traceId = "none";
+        var span = tracer.currentSpan();
+        if (span != null && span.context() != null) {
+            traceId = span.context().traceIdString();
+        }
+        return id.withTag(Tag.of("traceId", traceId));
+      }
+    };
   }
 
+  @Bean
+  public TimedAspect timedAspect(MeterRegistry registry) {
+    return new TimedAspect(registry);
+  }
 }
